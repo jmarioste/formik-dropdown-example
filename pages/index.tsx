79d78635df2f7ecd5595b76d6822e628b400e2fa
Jasser Mark Arioste <jassermark.arioste@gmail.com>
2022-12-28 10:28:48 +0800

    Add h1

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,9 @@ import React from "react";
 const HomePage: NextPage = () => {
   return (
     <div className="container">
-      <div className="grid place-content-center min-h-screen"></div>
+      <div className="grid place-content-center min-h-screen">
+        <h1>Formik Dropdown Example</h1>
+      </div>
     </div>
   );
 };
