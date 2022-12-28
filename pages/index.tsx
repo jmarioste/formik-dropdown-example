06e029d82b0e53dba981c38a9b0bbae6d7f3b70a
Jasser Mark Arioste <jassermark.arioste@gmail.com>
2022-12-28 10:28:14 +0800

    Initial commit from Create Next App

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from "next";
+import React from "react";
+
+const HomePage: NextPage = () => {
+  return (
+    <div className="container">
+      <div className="grid place-content-center min-h-screen"></div>
+    </div>
+  );
+};
+
+export default HomePage;
